1f0cb5fdcacf3bd232f209cc9b389c909b1f7110
Christopher Magtibay <cagmagtibay@live.com>
2023-09-12 18:28:49 -0400

    Create (002)reverseString.js

---
+++ JSAlgorithmsAndDataStructures/BasicAlgorithmScripting/(002)reverseString.js
@@ -0,0 +1,6 @@
+// This function takes in a string as input and returns a reversed version of the input string.
+function reverseString(str) {
+  return str.split("").reverse().join("");
+}
+
+reverseString("hello");
