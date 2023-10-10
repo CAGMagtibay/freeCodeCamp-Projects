8e79b908eb41fff1d768079d22a05a43f57d9d4b
Christopher Magtibay <cagmagtibay@live.com>
2023-10-10 12:09:21 -0400

    Create (019)makeAPerson.js

---
+++ JSAlgorithmsAndDataStructures/IntermediateAlgorithmScripting/(019)makeAPerson.js
@@ -0,0 +1,31 @@
+// This is a constructor that creates a Person instance
+const Person = function(first, last) {
+  let firstName = first;
+  let lastName = last;
+
+  this.getFullName = function() {
+    return this.getFirstName() + " " + this.getLastName();
+  };
+
+  this.getFirstName = function() {
+    return firstName;
+  }
+
+  this.getLastName = function() {
+    return lastName;
+  }
+
+  this.setFirstName = function(first) {
+    return firstName = first;
+  }
+
+  this.setLastName = function(last) {
+    return lastName = last;
+  }
+
+  this.setFullName = function(first, last) {
+    this.setFirstName(first);
+    this.setLastName(last);
+    return this.getFullName();
+  }
+};
